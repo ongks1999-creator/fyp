ce1cdacab795e4516705a97b67dea4838b378c64
ko25 <ko25@imperial.ac.uk>
2026-07-16 20:56:07 +0100

    refined the prompt for claim extraction

--- llama_nuclear_dataset_creation.py
+++ llama_nuclear_dataset_creation.py
@@ -37,7 +37,8 @@ def call_llama_api(prompt: str):
 def claim_extraction_from_chunk(chunk: list):
 
     chunk_text = chunk["chunk"]
-    prompt = f"Extract an atomic claim from the following text: {chunk_text}. From this atomic claim, extract the company name. Return ONLY a JSON object with ONLY these two keys: \"claim\" and \"company\". If there is no company mentioned in the claim, return a None under \"company\" For example, {{\"claim\": \"ABC plans to build two new SMR reactors\", \"company\": \"ABC\"}}"
+    # refined prompt to ensure nuclear claims are complete, and company name given a null for json format
+    prompt = f"Extract an atomic claim relating to the nuclear domain from the following text: {chunk_text}. The claim MUST be complete and verifiable. From this atomic claim, extract the company name. Return ONLY a JSON object with ONLY these two keys: \"claim\" and \"company\". If there is no company mentioned in the claim, return a null value under \"company\" For example, {{\"claim\": \"ABC plans to build two new SMR reactors\", \"company\": \"ABC\"}}"
 
     claim_output = call_llama_api(prompt)
 
