c7484f04f95c1043aba7e3d49e16009e2f9d72e0
ko25 <ko25@imperial.ac.uk>
2026-07-20 22:26:53 +0100

    added code to filter articles

---
+++ article_filter.py
@@ -0,0 +1,55 @@
+import json
+from chunk_index import url_normalisation, create_source_id
+
+def read_json_file(filepath: str) -> list:
+
+    with open(filepath, "r") as file:
+        # jsonl file has one json object per line
+        data = [json.loads(entry) for entry in file]
+
+
+    return data
+
+def filter_articles(post_data, comments_data):
+    """
+    Filter articles that contain "small modular reactor" or "smr" in title or body text
+    If so, add the comments under that post to the overall text of the article
+    """
+    filtered_data = []
+    for entry in post_data:
+        if "small modular reactor" in entry["title"].lower() or "small modular reactor" in entry["selftext"].lower() or "smr" in entry["title"].lower() or "smr" in entry["selftext"].lower():
+            new_entry = {}
+            url = "https://reddit.com" + entry["permalink"] # since permalink does not have the reddit.com
+            new_entry["source_id"] = create_source_id(url)
+            new_entry["url"] = url_normalisation(url)
+            new_entry["title"] = entry["title"]
+            new_entry["magasine"] = "Reddit" # standardise to "magasine" for my paragraph extraction pipeline 
+            new_entry["date"] = entry["created_utc"]
+            text = [entry["selftext"]]
+            for comment in comments_data:
+                if comment["link_id"][3:] == entry["id"]:
+                    text.append(comment["body"]) # list of strings, for standardisation for paragraph extraction code
+
+            new_entry["text"] = text
+
+            filtered_data.append(new_entry)
+
+
+    return filtered_data
+
+
+def main():
+    post_filepaths = ["/vol/bitbucket/ko25/reddit/r_energy_posts.jsonl", "/vol/bitbucket/ko25/reddit/r_nuclear_posts.jsonl", "/vol/bitbucket/ko25/reddit/r_NuclearPower_posts.jsonl"]
+    comments_filepaths = ["/vol/bitbucket/ko25/reddit/r_energy_comments.jsonl", "/vol/bitbucket/ko25/reddit/r_nuclear_comments.jsonl", "/vol/bitbucket/ko25/reddit/r_NuclearPower_comments.jsonl"]
+    filtered_articles = []
+    for post_filepath, comments_filepath in zip(post_filepaths, comments_filepaths):
+        post_data = read_json_file(post_filepath)
+        comments_data = read_json_file(comments_filepath)
+        filtered_articles.extend(filter_articles(post_data, comments_data))
+
+    with open("filtered_reddit_articles.json", "w") as file:
+        json.dump(filtered_articles, file, indent = 1)
+
+if __name__ == "__main__":
+    main()
+
