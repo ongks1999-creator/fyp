eb4ca5b92be3bf2c7a904534662a65b8c1194927
ko25 <ko25@imperial.ac.uk>
2026-07-22 00:36:12 +0100

    added chunks, sources csv (trial run) and updated extract pargraph code

--- extract_paragraphs.py
+++ extract_paragraphs.py
@@ -4,7 +4,7 @@ import pandas as pd
 import json
 
 # Since we using Scibert model, we use the same model for our tokenizer
-tokenizer = AutoTokenizer.from_pretrained("allenai/scibert_scivocab_uncased")
+tokenizer = AutoTokenizer.from_pretrained("/homes/ko25/Desktop/fyp/scibert_finetuned_model")
 
 def extract_paragraphs_from_article(article: dict) -> list[dict]:
     """
@@ -57,16 +57,15 @@ def create_paragraphs_csv(articles_filepath) -> None:
         dataframe_list.extend(paragraphs) # prevent adding the list of dict as an element
     
     df = pd.DataFrame(dataframe_list)
-    df.to_csv("paragraphs.csv", index = False) # write dataframe to csv
-
+    #df.to_csv("paragraphs.csv", index = False) # write dataframe to csv
+    df.to_csv("paragraphs_extra.csv", index = False)
     return
 
 if __name__ == "__main__":
-    file_path = "/Users/ongkaisheng/Desktop/ImperialCollege/FYP/finalcode/articles/filtered_articles.json"
+    file_path = "/homes/ko25/Desktop/fyp/filtered_reddit_articles.json" #"/Users/ongkaisheng/Desktop/ImperialCollege/FYP/finalcode/articles/filtered_articles.json"
     invalid_entries = entry_check(file_path)
     print(invalid_entries)
     print([invalid["magasine"] for invalid in invalid_entries])
     print(f"Number of invalid entries: {len(invalid_entries)}")
     create_paragraphs_csv(file_path)
-        
 
